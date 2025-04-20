111706c2b0e74f5fdd2750771384fb349da1525d
Gerard Ivalaoa <rardiva09@gmail.com>
2025-04-20 16:30:14 +1000

    Update jQuery.headroom.js

--- js/jQuery.headroom.js
+++ js/jQuery.headroom.js
@@ -1,31 +1,30 @@
-(function($) {
-
-  if(!$) {
-    return;
+(function($) { // Define a self-invoking function to allow the use of $ as a shorthand for jQuery or Zepto
+  if(!$) { // Check if $ is not defined (neither jQuery nor Zepto)
+    return; // Exit the function if $ is not available
   }
 
   ////////////
   // Plugin //
   ////////////
 
-  $.fn.headroom = function(option) {
-    return this.each(function() {
-      var $this   = $(this),
-        data      = $this.data('headroom'),
-        options   = typeof option === 'object' && option;
+  $.fn.headroom = function(option) { // Define a jQuery plugin called "headroom"
+    return this.each(function() { // Iterate over each element in the jQuery object
+      var $this   = $(this), // Create a jQuery object for the current element
+        data      = $this.data('headroom'), // Retrieve any existing data associated with the 'headroom' key
+        options   = typeof option === 'object' && option; // Check if the provided option is an object
 
-      options = $.extend(true, {}, Headroom.options, options);
+      options = $.extend(true, {}, Headroom.options, options); // Merge default options with provided options
 
-      if (!data) {
-        data = new Headroom(this, options);
-        data.init();
-        $this.data('headroom', data);
+      if (!data) { // If there is no existing headroom data for the element
+        data = new Headroom(this, options); // Create a new Headroom instance for the current element
+        data.init(); // Initialize the headroom functionality
+        $this.data('headroom', data); // Store the headroom instance in the element’s data
       }
-      if (typeof option === 'string') {
-        data[option]();
+      if (typeof option === 'string') { // If the option is a string (indicating a method to call)
+        data[option](); // Call the specified method on the headroom instance
 
-        if(option === 'destroy'){
-          $this.removeData('headroom');
+        if(option === 'destroy'){ // If the method is 'destroy'
+          $this.removeData('headroom'); // Remove the headroom data from the element
         }
       }
     });
@@ -35,9 +34,9 @@
   // Data API //
   //////////////
 
-  $('[data-headroom]').each(function() {
-    var $this = $(this);
-    $this.headroom($this.data());
+  $('[data-headroom]').each(function() { // Select all elements with the data-headroom attribute
+    var $this = $(this); // Create a jQuery object for the current element
+    $this.headroom($this.data()); // Initialize the headroom plugin using the element's data attributes
   });
 
-}(window.Zepto || window.jQuery));
+}(window.Zepto || window.jQuery)); // Execute the function using Zepto if available, otherwise jQuery
